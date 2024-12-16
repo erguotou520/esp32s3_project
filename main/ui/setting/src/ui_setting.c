93326ffc9629d5bb96f9e7fa8f3804c5fce564d3
erguotou <erguotou525@gmail.com>
2024-12-16 23:54:07 +0800

    chore: update

--- main/ui/setting/src/ui_setting.c
+++ main/ui/setting/src/ui_setting.c
@@ -2,12 +2,13 @@
 #include "ui_common.h"
 #include "page_manager.h"
 
-typedef enum {
+enum {
     LV_MENU_ITEM_BUILDER_VARIANT_1,
     LV_MENU_ITEM_BUILDER_VARIANT_2
-} lv_menu_builder_variant_t;
+};
+typedef uint8_t lv_menu_builder_variant_t;
 
-static void switch_handler(lv_event_t * e);
+static void back_event_handler(lv_event_t * e);
 lv_obj_t * root_page;
 static lv_obj_t * create_text(lv_obj_t * parent, const char * icon, const char * txt,
                               lv_menu_builder_variant_t builder_variant);
@@ -18,89 +19,83 @@ static lv_obj_t * create_switch(lv_obj_t * parent,
 
 void lv_setting_page(void)
 {
-    lv_obj_t * menu = lv_menu_create(lv_screen_active());
+    lv_obj_t * menu = lv_menu_create(lv_scr_act());
+    lv_obj_set_style_radius(menu, 10, 0);
+    lv_obj_t * back_btn = lv_menu_get_main_header_back_btn(menu);
+    lv_obj_set_size(back_btn, 40, 40);
+    lv_obj_t * header = lv_menu_get_main_header(menu);
+    // 获取标题标签对象（通常是header的第二个子对象，因为第一个是返回按钮）
+    lv_obj_t * title = lv_obj_get_child(header, 1);
+    if(title) {
+        // 设置字体大小
+        lv_obj_set_style_text_font(title, &font_alipuhui20, 0);  // 使用20号中文字体
+        
+        // 设置文字颜色
+        lv_obj_set_style_text_color(title, lv_color_hex(0x000000), 0);
+        
+        // 设置文字对齐方式
+        lv_obj_set_style_text_align(title, LV_TEXT_ALIGN_CENTER, 0);
+        
+        // 调整标题位置
+        lv_obj_align(title, LV_ALIGN_CENTER, 0, 0);
+    }
 
-    lv_menu_set_mode_root_back_button(menu, LV_MENU_ROOT_BACK_BTN_ENABLED);
-    lv_obj_add_event_cb(menu, navigate_back, LV_EVENT_CLICKED, menu);
-    lv_obj_set_size(menu, lv_display_get_horizontal_resolution(NULL), lv_display_get_vertical_resolution(NULL));
+    lv_color_t bg_color = lv_obj_get_style_bg_color(menu, 0);
+    if(lv_color_brightness(bg_color) > 127) {
+        lv_obj_set_style_bg_color(menu, lv_color_darken(lv_obj_get_style_bg_color(menu, 0), 10), 0);
+    }
+    else {
+        lv_obj_set_style_bg_color(menu, lv_color_darken(lv_obj_get_style_bg_color(menu, 0), 50), 0);
+    }
+    lv_menu_set_mode_root_back_btn(menu, LV_MENU_ROOT_BACK_BTN_ENABLED);
+    lv_obj_add_event_cb(menu, back_event_handler, LV_EVENT_CLICKED, menu);
+    lv_obj_set_size(menu, LV_HOR_RES, LV_VER_RES);
     lv_obj_center(menu);
 
     lv_obj_t * cont;
     lv_obj_t * section;
 
-    /*Create sub pages*/
-    lv_obj_t * wifi_sub_page = lv_menu_page_create(menu, NULL);
-    lv_obj_set_style_pad_hor(wifi_sub_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
-    lv_menu_separator_create(wifi_sub_page);
-    section = lv_menu_section_create(wifi_sub_page);
-    create_slider(section, LV_SYMBOL_SETTINGS, "Velocity", 0, 150, 120);
-    create_slider(section, LV_SYMBOL_SETTINGS, "Acceleration", 0, 150, 50);
-    create_slider(section, LV_SYMBOL_SETTINGS, "Weight limit", 0, 150, 80);
+    // 创建子页面
+    lv_obj_t * sub_wifi_page = lv_menu_page_create(menu, NULL);
+    lv_obj_set_style_pad_hor(sub_wifi_page, 0, 0);
+    lv_menu_separator_create(sub_wifi_page);
+    section = lv_menu_section_create(sub_wifi_page);
+    create_switch(section,  NULL, "启用 WIFI", true);
 
-    lv_obj_t * sub_sound_page = lv_menu_page_create(menu, NULL);
-    lv_obj_set_style_pad_hor(sub_sound_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
-    lv_menu_separator_create(sub_sound_page);
-    section = lv_menu_section_create(sub_sound_page);
-    create_switch(section, LV_SYMBOL_AUDIO, "Sound", false);
+    lv_obj_t * sub_ble_page = lv_menu_page_create(menu, NULL);
+    lv_obj_set_style_pad_hor(sub_ble_page, 0, 0);
+    lv_menu_separator_create(sub_ble_page);
+    section = lv_menu_section_create(sub_ble_page);
+    create_switch(section, NULL, "启用蓝牙", false);
 
     lv_obj_t * sub_display_page = lv_menu_page_create(menu, NULL);
-    lv_obj_set_style_pad_hor(sub_display_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
+    lv_obj_set_style_pad_hor(sub_display_page, 0, 0);
     lv_menu_separator_create(sub_display_page);
     section = lv_menu_section_create(sub_display_page);
-    create_slider(section, LV_SYMBOL_SETTINGS, "Brightness", 0, 150, 100);
-
-    lv_obj_t * sub_software_info_page = lv_menu_page_create(menu, NULL);
-    lv_obj_set_style_pad_hor(sub_software_info_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
-    section = lv_menu_section_create(sub_software_info_page);
-    create_text(section, NULL, "Version 1.0", LV_MENU_ITEM_BUILDER_VARIANT_1);
-
-    lv_obj_t * sub_legal_info_page = lv_menu_page_create(menu, NULL);
-    lv_obj_set_style_pad_hor(sub_legal_info_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
-    section = lv_menu_section_create(sub_legal_info_page);
-    for(uint32_t i = 0; i < 15; i++) {
-        create_text(section, NULL,
-                    "This is a long long long long long long long long long text, if it is long enough it may scroll.",
-                    LV_MENU_ITEM_BUILDER_VARIANT_1);
-    }
+    create_switch(section, NULL, "开启背光", false);
 
     lv_obj_t * sub_about_page = lv_menu_page_create(menu, NULL);
-    lv_obj_set_style_pad_hor(sub_about_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
-    lv_menu_separator_create(sub_about_page);
+    lv_obj_set_style_pad_hor(sub_about_page, 0, 0);
     section = lv_menu_section_create(sub_about_page);
-    cont = create_text(section, NULL, "Software information", LV_MENU_ITEM_BUILDER_VARIANT_1);
-    lv_menu_set_load_page_event(menu, cont, sub_software_info_page);
-    cont = create_text(section, NULL, "Legal information", LV_MENU_ITEM_BUILDER_VARIANT_1);
-    lv_menu_set_load_page_event(menu, cont, sub_legal_info_page);
+    create_text(section, NULL, "Version 1.0", LV_MENU_ITEM_BUILDER_VARIANT_1);
 
-    lv_obj_t * sub_menu_mode_page = lv_menu_page_create(menu, NULL);
-    lv_obj_set_style_pad_hor(sub_menu_mode_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
-    lv_menu_separator_create(sub_menu_mode_page);
-    section = lv_menu_section_create(sub_menu_mode_page);
-    cont = create_switch(section, LV_SYMBOL_AUDIO, "Sidebar enable", true);
-    lv_obj_add_event_cb(lv_obj_get_child(cont, 2), switch_handler, LV_EVENT_VALUE_CHANGED, menu);
-
-    /*Create a root page*/
-    root_page = lv_menu_page_create(menu, "Settings");
-    lv_obj_set_style_pad_hor(root_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
+    // 创建主页面
+    root_page = lv_menu_page_create(menu, "设置");
+    lv_obj_set_style_pad_hor(root_page, 0, 0);
+    lv_obj_set_size(root_page, 120, LV_VER_RES);
     section = lv_menu_section_create(root_page);
-    cont = create_text(section, LV_SYMBOL_SETTINGS, "Mechanics", LV_MENU_ITEM_BUILDER_VARIANT_1);
-    lv_menu_set_load_page_event(menu, cont, wifi_sub_page);
-    cont = create_text(section, LV_SYMBOL_AUDIO, "Sound", LV_MENU_ITEM_BUILDER_VARIANT_1);
-    lv_menu_set_load_page_event(menu, cont, sub_sound_page);
-    cont = create_text(section, LV_SYMBOL_SETTINGS, "Display", LV_MENU_ITEM_BUILDER_VARIANT_1);
+    cont = create_text(section, NULL, "WIFI", LV_MENU_ITEM_BUILDER_VARIANT_1);
+    lv_menu_set_load_page_event(menu, cont, sub_wifi_page);
+    cont = create_text(section, NULL, "蓝牙", LV_MENU_ITEM_BUILDER_VARIANT_1);
+    lv_menu_set_load_page_event(menu, cont, sub_ble_page);
+    cont = create_text(section, NULL, "背光", LV_MENU_ITEM_BUILDER_VARIANT_1);
     lv_menu_set_load_page_event(menu, cont, sub_display_page);
-
-    create_text(root_page, NULL, "Others", LV_MENU_ITEM_BUILDER_VARIANT_1);
-    section = lv_menu_section_create(root_page);
-    cont = create_text(section, NULL, "About", LV_MENU_ITEM_BUILDER_VARIANT_1);
+    cont = create_text(section, NULL, "关于", LV_MENU_ITEM_BUILDER_VARIANT_1);
     lv_menu_set_load_page_event(menu, cont, sub_about_page);
-    cont = create_text(section, LV_SYMBOL_SETTINGS, "Menu mode", LV_MENU_ITEM_BUILDER_VARIANT_1);
-    lv_menu_set_load_page_event(menu, cont, sub_menu_mode_page);
 
     lv_menu_set_sidebar_page(menu, root_page);
 
-    lv_obj_send_event(lv_obj_get_child(lv_obj_get_child(lv_menu_get_cur_sidebar_page(menu), 0), 0), LV_EVENT_CLICKED,
-                      NULL);
+    lv_event_send(lv_obj_get_child(lv_obj_get_child(lv_menu_get_cur_sidebar_page(menu), 0), 0), LV_EVENT_CLICKED, NULL);
 }
 
 static void back_event_handler(lv_event_t * e)
@@ -108,31 +103,8 @@ static void back_event_handler(lv_event_t * e)
     lv_obj_t * obj = lv_event_get_target(e);
     lv_obj_t * menu = lv_event_get_user_data(e);
 
-    if(lv_menu_back_button_is_root(menu, obj)) {
-        lv_obj_t * mbox1 = lv_msgbox_create(NULL);
-        lv_msgbox_add_title(mbox1, "Hello");
-        lv_msgbox_add_text(mbox1, "Root back btn click.");
-        lv_msgbox_add_close_button(mbox1);
-    }
-}
-
-static void switch_handler(lv_event_t * e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * menu = lv_event_get_user_data(e);
-    lv_obj_t * obj = lv_event_get_target(e);
-    if(code == LV_EVENT_VALUE_CHANGED) {
-        if(lv_obj_has_state(obj, LV_STATE_CHECKED)) {
-            lv_menu_set_page(menu, NULL);
-            lv_menu_set_sidebar_page(menu, root_page);
-            lv_obj_send_event(lv_obj_get_child(lv_obj_get_child(lv_menu_get_cur_sidebar_page(menu), 0), 0), LV_EVENT_CLICKED,
-                              NULL);
-        }
-        else {
-            lv_menu_set_sidebar_page(menu, NULL);
-            lv_menu_clear_history(menu); /* Clear history because we will be showing the root page later */
-            lv_menu_set_page(menu, root_page);
-        }
+    if(lv_menu_back_btn_is_root(menu, obj)) {
+        navigate_back();
     }
 }
 
@@ -145,14 +117,14 @@ static lv_obj_t * create_text(lv_obj_t * parent, const char * icon, const char *
     lv_obj_t * label = NULL;
 
     if(icon) {
-        img = lv_image_create(obj);
-        lv_image_set_src(img, icon);
+        img = lv_img_create(obj);
+        lv_img_set_src(img, icon);
     }
 
     if(txt) {
         label = lv_label_create(obj);
         lv_label_set_text(label, txt);
-        lv_label_set_long_mode(label, LV_LABEL_LONG_MODE_SCROLL_CIRCULAR);
+        lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
         lv_obj_set_flex_grow(label, 1);
     }
 
